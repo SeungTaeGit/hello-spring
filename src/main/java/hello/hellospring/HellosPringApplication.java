23bcb6561f4b72814e0a57fd54edeb0cd1367b50
SeungTae <dltmdxo1589@daum.net>
2024-01-12 22:42:27 +0900

    Initial commit

---
+++ src/main/java/hello/hellospring/HellosPringApplication.java
@@ -0,0 +1,13 @@
+package hello.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellosPringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellosPringApplication.class, args);
+	}
+
+}
